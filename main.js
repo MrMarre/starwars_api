d6874ebd48abc2bc9aa1788eab1f49254530b351
antonisoaho <isoahoanton@gmail.com>
2023-11-20 11:10:59 +0100

    Oke

--- main.js
+++ main.js
@@ -1,48 +1,9 @@
-const baseURL = `https://swapi.dev/api/people/?page=`
-const page = `1`;
+const baseURL = `https://swapi.dev/api/people/`;
 
-const characters = document.querySelector(`#characters`);
+const pageViewer = () => {
+  return parseInt(document.querySelector('footer').textContent);
+};
 
+let page = pageViewer();
 
-
- fetch(`${baseURL}${page}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        } return response.json();
-    })
-    .then(data => {
-
-
-        data.results.forEach(person => {
-            console.log(person.name)
-            const character = document.createElement("div")
-            character.textContent = person.name;
-            characters.appendChild(character);
-            
-        
-            characterSelection = document.querySelectorAll(`#characters div`)
-            
-
-            characterSelection.forEach(char => {
-
-                char.addEventListener("click", function() {
-                    const siblings = Array.from(this.parentNode.childNodes)
-                    console.log(siblings.length)
-                })
-
-            })
-
-
-
-         })})
-        // console.log(data)})
-     //console.log(data.results)})
-     
-
-    .catch(error => console.log(`An error was found: `, error)) 
-
-
-
-
-
+console.log(page);
