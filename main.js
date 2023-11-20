90e843ad9dc2055ce9c9a49ee489a51d8bc9a8f2
antonisoaho <isoahoanton@gmail.com>
2023-11-20 11:15:34 +0100

    Status committing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const baseURL = `https://swapi.dev/api/people/?page=`;
-const page = `1`;
+let page;
 
 const pageViewer = () => {
   return parseInt(document.querySelector('footer').textContent);
@@ -8,7 +8,17 @@ const pageViewer = () => {
 const characters = document.querySelectorAll('#characters li');
 
 const dataFetching = async (pageNumber) => {
-  fetch(`${baseURL}?page=${page}`);
+  fetch(`${baseURL}?page=${page}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Error in fetching data', +response.statusText);
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
 };
 
 page = pageViewer();
