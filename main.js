e4a36ec9c8ff5a5d5d23416e6a21f233e99e73f9
Martin <martin.kolberg@outlook.com>
2023-11-20 11:14:40 +0100

    new commit

--- main.js
+++ main.js
@@ -14,7 +14,16 @@ const characters = document.querySelectorAll("#characters li");
 const dataFetching = async(pageNumber) => {
 
 fetch(`${baseURL}?page=${page}`)
-
+    .then(response => {
+        if (!response.ok){
+            throw new Error("Error in fetching data", + response.statusText)
+        } else {
+            return response.json()
+        }
+    }) 
+    .then (data => {
+        console.log(data);
+    })
 } 
 
  
