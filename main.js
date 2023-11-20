1788f2c060066759adc1ac05423ed61289e51e52
Martin <martin.kolberg@outlook.com>
2023-11-20 11:10:37 +0100

    Keeping us up to date

--- main.js
+++ main.js
@@ -1,48 +1,26 @@
-const baseURL = `https://swapi.dev/api/people/?page=`
-const page = `1`;
+const baseURL = `https://swapi.dev/api/people/`
+let page;
 
-const characters = document.querySelector(`#characters`);
-
-
-
- fetch(`${baseURL}${page}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        } return response.json();
-    })
-    .then(data => {
-
-
-        data.results.forEach(person => {
-            console.log(person.name)
-            const character = document.createElement("div")
-            character.textContent = person.name;
-            characters.appendChild(character);
-            
-        
-            characterSelection = document.querySelectorAll(`#characters div`)
-            
-
-            characterSelection.forEach(char => {
-
-                char.addEventListener("click", function() {
-                    const siblings = Array.from(this.parentNode.childNodes)
-                    console.log(siblings.length)
-                })
-
-            })
+const pageViewer = () => {
+    return parseInt(document.querySelector('footer').textContent);
+  };
+    
+    
 
 
 
-         })})
-        // console.log(data)})
-     //console.log(data.results)})
-     
+const characters = document.querySelectorAll("#characters li"); 
 
-    .catch(error => console.log(`An error was found: `, error)) 
+const dataFetching = async(pageNumber) => {
+
+fetch(`${baseURL}?page=${page}`)
+
+} 
+
+ 
+    
 
 
 
 
-
+page = pageViewer()
