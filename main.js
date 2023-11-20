17d622c0fb4b746330cfaf5a8e0ddbfc1c412619
antonisoaho <isoahoanton@gmail.com>
2023-11-20 16:35:21 +0100

    Pushing final features on javascript

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+// Defining DOM-elements
 const baseURL = 'https://swapi.dev/api/people/';
 const characters = document.querySelectorAll('#characters li');
 const buttons = document.querySelectorAll('input');
-const detailLists = document.querySelectorAll('#details footer ul');
+const detailLists = document.querySelectorAll('.details footer ul');
+const pageSwappers = document.querySelectorAll('.page-swapper');
 
+// Creation of global variables
 let page;
 let characterInfo;
 
+// Function to properly handle different pages
 const pageViewer = () => {
-  return parseInt(document.querySelector('footer').textContent);
+  return parseInt(document.querySelector('#page-index').textContent);
 };
 
+// Function to fetch data based on pagenumber
 const dataFetching = async (pageNumber) => {
   fetch(`${baseURL}?page=${page}`)
     .then((response) => {
@@ -20,7 +25,7 @@ const dataFetching = async (pageNumber) => {
       }
     })
     .then((data) => {
-      characterInfo = data.results;
+      characterInfo = data.results; // Results is a key inside the object
       printOutNames(characters, characterInfo);
       return characterInfo;
     })
@@ -29,12 +34,14 @@ const dataFetching = async (pageNumber) => {
     });
 };
 
+// Function to render all character-names to list
 const printOutNames = (characters, names) => {
   names.forEach((name, index) => {
     characters[index].textContent = name.name;
   });
 };
 
+// Function to print out all the basic information about chosen character
 const printOutInfo = (character) => {
   const infoList = document.querySelectorAll('#details ul li span');
   document.querySelector('#details h2').textContent = character.name;
@@ -42,25 +49,74 @@ const printOutInfo = (character) => {
     listItem.textContent = character[listItem.id];
   });
 };
-
+// From the function above we call this to get the proper fetch-method and then printing it out in the correct field
 const printOutAssets = (character) => {
+  // Clear all the fields from old data
+  const listItemsToClear = Array.from(
+    document.querySelectorAll(
+      '.details footer ul h3, .details footer ul li span'
+    )
+  );
+  listItemsToClear.forEach((item) => {
+    item.textContent = '';
+  });
+  // Alot of ifs to find where the correct data should be placed and which to see.
   buttons.forEach((btn) => {
     const fieldOfChoice = btn.id;
+
     if (character[fieldOfChoice].length > 0) {
       if (Array.isArray(character[fieldOfChoice])) {
         character[fieldOfChoice].forEach((item) => {
-          fetchAssets(item);
+          fetchAssets(item, fieldOfChoice);
         });
       } else {
+        fetchAssets(character[fieldOfChoice], fieldOfChoice);
       }
     } else {
-      console.log('Ingen data');
+      const ulFieldName = document.querySelector(
+        `article footer .${fieldOfChoice} .name`
+      );
+      const listItems = document.querySelectorAll(
+        `article footer .${fieldOfChoice} li span`
+      );
+
+      ulFieldName.textContent = 'N/A';
+      listItems.forEach((field) => {
+        field.textContent = 'N/A';
+      });
     }
   });
 };
 
-const fetchAssets = async (link) => {};
+// Sort and push detail-data from the chosen character
+const fetchAssets = async (link, specifics) => {
+  fetch(link)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Server error:', +response.statusText);
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      const ulFieldName = document.querySelector(
+        `article footer .${specifics} .name`
+      );
+      const listItems = document.querySelectorAll(
+        `article footer .${specifics} li span`
+      );
 
+      // If the field has data then use ',' else just put the data
+      ulFieldName.textContent +=
+        (ulFieldName.textContent ? ', ' : '') + data.name;
+      listItems.forEach((field) => {
+        field.textContent += (field.textContent ? ', ' : '') + data[field.id];
+      });
+    })
+    .catch((error) => console.log(error));
+};
+
+// EventListener to choose which character to see the details about
 characters.forEach((character) => {
   character.addEventListener('click', function () {
     charName = this.textContent;
@@ -71,6 +127,7 @@ characters.forEach((character) => {
   });
 });
 
+// EventListener to hide and show different type of details
 buttons.forEach((btn) => {
   btn.addEventListener('click', () => {
     if (btn.checked) {
@@ -82,6 +139,19 @@ buttons.forEach((btn) => {
         }
       });
     }
+  });
+});
+
+// EventListener to handle pages
+pageSwappers.forEach((swapper) => {
+  swapper.addEventListener('click', () => {
+    if (swapper.id === 'higher') {
+      page++;
+    } else {
+      page--;
+    }
+    dataFetching(page);
+    document.querySelector('#page-index').textContent = page;
   });
 });
 
